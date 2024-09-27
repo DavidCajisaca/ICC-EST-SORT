ba58e4cceae88386451a2bcce2fe9d41cda36639
Dennis Cajisaca <cajisacadennis@gmail.com>
2024-09-27 12:19:33 -0500

    Seleccion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Metodo de seleccion");
+        MetodosOrdenamiento m=new MetodosOrdenamiento();
+        int [] arreglosd={9, 5, 30, 10, 24};
+        int[] arregloorde=m.seleccion(arreglosd);
+        m.printarreglo(arregloorde);
+        System.out.println(" ");
     }
 }
---
+++ src/MetodosOrdenamiento.java
@@ -0,0 +1,24 @@
+public class MetodosOrdenamiento {
+    public int[] seleccion(int[] arreglo) {
+        int tamano = arreglo.length;
+        for (int i = 0; i < tamano - 1; i++) {
+            int indice = i;
+            for (int j = i + 1; j < tamano; j++) {
+                if (arreglo[j] < arreglo[indice]) {  
+                    indice = j;
+                }
+            }
+            int aux = arreglo[indice];
+            arreglo[indice] = arreglo[i];
+            arreglo[i] = aux;
+        }
+        return arreglo;
+    }
+
+    // Method to print the array
+    public void printarreglo(int[] arreglos) {
+        for (int elem : arreglos) {
+            System.out.print(" "+elem);
+        }
+    }
+}
